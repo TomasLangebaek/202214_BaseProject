8545f44532e3e58026f29e2ccff5d194b946557c
TomasLangebaek <t.langebaek@uniandes.edu.co>
2022-09-17 12:28:19 -0500

    implementar persistencia

---
+++ src/producto/producto.entity.ts
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { TiendaEntity } from "../tienda/tienda.entity";
+
+@Entity()
+export class ProductoEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  nombre: string;
+
+  @Column()
+  precio: string;
+
+  @Column()
+  tipo: string;
+
+  @ManyToMany(() => TiendaEntity, tienda => tienda.productos)
+  tiendas: TiendaEntity[];
+}
